688dbc74550bc140cff193546ad5b84af09b633c
Jacques <gkounkou@gmail.com>
2017-10-14 16:23:25 +0200

    Update test.c

--- lab/test.c
+++ lab/test.c
@@ -4,11 +4,24 @@
  */
 
 #include <avr/io.h>
-#include "test.h"
+#include <util/delay.h>
 
-int var;
-
-int main (int argc, char **argv)
+int main (void)
 {
-    return var + SP;
+   uint8_t leds = 1;
+
+   DDRB = 0xFF;
+
+   while(1) {
+      for (leds = 1; leds <= (1 << 5); leds <<= 1) {
+         PORTB = leds;
+         _delay_ms(200);
+      }   
+      for (leds = (1 << 4); leds >= 2; leds >>= 1) {
+         PORTB = leds;
+         _delay_ms(200);
+      }   
+   }   
+
+   return 0;
 }
